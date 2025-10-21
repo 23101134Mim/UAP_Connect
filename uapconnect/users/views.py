dab0c2ee7f49696f74eafe83154ee6eaf3e97456
23101134Mim <23101134@uap-bd.edu>
2025-10-21 17:37:43 +0600

    user app: views.py

--- uapconnect/users/views.py
+++ uapconnect/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('login')
+
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
