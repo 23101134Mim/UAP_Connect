c72fbb00f8f5dc1662e9f4b9871e8d483caa4f52
sujana120 <23101120@uap-bd.edu>
2025-10-22 17:20:02 +0600

    Academics app models.py

--- uapconnect/academics/models.py
+++ uapconnect/academics/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from users.models import Student
+
+class Course(models.Model):
+    code = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.code
+
+class ClassRoutine(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    day = models.CharField(max_length=10)
+    time = models.CharField(max_length=20)
+    room = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return f"{self.course.code} - {self.day}"
+
+class Mark(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    ct1 = models.IntegerField()
+    ct2 = models.IntegerField()
+    mid = models.IntegerField()
+    final = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student.user.username} - {self.course.code}"
