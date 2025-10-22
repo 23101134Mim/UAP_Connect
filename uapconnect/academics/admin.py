ded686b11096e4cb70fa526ac099018eacf2550c
sujana120 <23101120@uap-bd.edu>
2025-10-22 17:22:54 +0600

    Academics app admin.py

--- uapconnect/academics/admin.py
+++ uapconnect/academics/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Course, ClassRoutine, Mark
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title')
+
+@admin.register(ClassRoutine)
+class ClassRoutineAdmin(admin.ModelAdmin):
+    list_display = ('course', 'day', 'time', 'room')
+
+@admin.register(Mark)
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'ct1', 'ct2', 'mid', 'final')
