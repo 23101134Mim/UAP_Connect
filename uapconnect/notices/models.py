c493d8cc7ff3e267d1e6e4ba1abe066abcac5476
Tina05-02 <tinabadrunnesa@gmail.com>
2025-10-23 00:47:41 +0600

    Notices app: models.py

--- uapconnect/notices/models.py
+++ uapconnect/notices/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class NoticeCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(NoticeCategory, on_delete=models.CASCADE)
+    description = models.TextField()
+    file = models.FileField(upload_to='notices/', blank=True, null=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
