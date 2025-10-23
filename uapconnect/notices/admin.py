517ab40d1be2cc5aa11ab9e96e086de7298e0067
tamimiqbal <tamimiqbal.connect@gmail.com>
2025-10-23 15:35:54 +0600

    notice app admin.py

--- uapconnect/notices/admin.py
+++ uapconnect/notices/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import NoticeCategory, Notice
+
+@admin.register(NoticeCategory)
+class NoticeCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'posted_at')
