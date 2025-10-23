00c55ff32c469328e3b65dcabf5cb07107263638
tamimiqbal <tamimiqbal.connect@gmail.com>
2025-10-23 13:15:03 +0600

    notice app views.py

--- uapconnect/notices/views.py
+++ uapconnect/notices/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Notice
+from .forms import NoticeForm
+
+def notice_list(request):
+    notices = Notice.objects.all()
+    return render(request, 'notices/notice_list.html', {'notices': notices})
+
+def notice_create(request):
+    form = NoticeForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('notice_list')
+    return render(request, 'notices/notice_form.html', {'form': form})
